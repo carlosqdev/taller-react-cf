3dd0f92d35d32beea64f1f48735b001fb1731603
carlosqdev <carlosquintero95nic@gmail.com>
2022-02-06 19:21:15 -0600

    Feat: I added image and class css

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import image from "../../assets/RickAndMorty.jpg"
+import "./Home.css";
 
 function Home() {
-  return <div>Hola</div>;
+  return (
+    <div className='container'>
+      <img
+        className="img"
+        src={image}
+        alt="Rick and Morty"
+        width="100%" />
+    </div>
+  );
 }
 
 export default Home;
