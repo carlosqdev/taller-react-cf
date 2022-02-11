e682bf8b8bf2d218df7e1bccb71101f41693f90d
carlosqdev <carlosquintero95nic@gmail.com>
2022-02-11 09:08:06 -0600

    Styles: I added styles and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import RoutesIndex from "./routes";
+import Footer from "./Components/Footer/Footer";
+
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="container__app">
       <RoutesIndex />
-    </>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>Proyecto creado para el taller de Código Facilito.</p>
+    </footer>
+  )
+}
+
+export default Footer
